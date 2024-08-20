086c132ef0f442f13dfe0c706330cf4977e526f1
mapulecodes <mbuzwanamapule@gmail.com>
2024-08-20 14:10:28 +0200

    FolderStructre

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,6 @@
-import { createStore } from 'vuex'
-
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+import { createStore } from 'vuex';
+import axios from 'axios';
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+import router from '@/router';
+import { useCookies } from 'vue3-cookies';
