44b6ebd1d9e5a61abbac27a839b703fe629bf1de
mapulecodes <mbuzwanamapule@gmail.com>
2024-08-20 16:00:31 +0200

    update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,47 +1,65 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
-        <div class="container-fluid">
-            <router-link class="navbar-brand" to="/">
-                <img src="" loading="lazy" alt="logo">
-            </router-link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/about">About</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/products">Products</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/checkout">Checkout</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/admin">Admin</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/reachUs">Contact Us</router-link>
-                    </li>
-                    <ul class="userAccount navbar-nav">
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/login">Login</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/logout">Logout</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/signup">Register</router-link>
-                        </li>
-                    </ul>
-                </ul>
-            </div>
+    <nav class="navbar navbar-expand-lg gradient-navbar sticky-top">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">
+          <img src="https://mapulecodes.github.io/fridayimages/images/mlogo.png" alt="logo" class="img-fluid logo" loading="lazy">
+        </router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#resume">Resume</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#project">Project</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#testimonial">Testimonial</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#reachMe">Reach Me</a>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
-</template>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .gradient-navbar {
+    background: linear-gradient(to right, #666e7a, #554671);
+  }
+  
+  .navbar-brand .logo {
+    width: 120px; 
+    height: auto;
+  }
+  
+  .navbar-nav {
+    margin-left: 10%; /* Adjust this value to move the nav content closer to the middle */
+  }
+  
+  .nav-link {
+    color: white !important;
+    font-size: 1.1rem;
+  }
+  
+  .navbar-toggler {
+    border: none;
+  }
+  
+  .navbar-toggler-icon {
+    background-image: url("data:image/svg+xml;charset=UTF8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3E%3Cpath stroke='rgba%288, 8, 8, 0.7%29' stroke-width='2' linecap='round' linejoin='round' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+  }
+  </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,1 @@
-import { createStore } from 'vuex';
-import axios from 'axios';
-import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
-import router from '@/router';
-import { useCookies } from 'vue3-cookies';
